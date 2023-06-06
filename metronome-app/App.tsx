e753ce4748b553c69f623e166a83edf466214d7a
Liene <Liene@LAPTOP-C1G7S8GH>
2023-06-06 10:45:53 +0300

    navigation bar v1

--- metronome-app/App.tsx
+++ metronome-app/App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { AntDesign } from "@expo/vector-icons";
+import { HomeScreen } from "./views/Home";
+import { AboutScreen } from "./views/About";
+import { SavedScreen } from "./views/Saved";
+import React from "react";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: any;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home";
+            } else if (route.name === "About") {
+              iconName = focused ? "questioncircle" : "questioncircleo";
+            } else if (route.name === "Saved") {
+              iconName = focused ? "pluscircle" : "pluscircleo";
+            }
+
+            return <AntDesign name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "black",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="About" component={AboutScreen} />
+        <Tab.Screen name="Saved" component={SavedScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ metronome-app/views/Saved.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export const SavedScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Saved!</Text>
+    </View>
+  );
+};
